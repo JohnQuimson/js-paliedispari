002b1faf64333c4bbd9d04932f8154a981e4487b
JohnQuimson <john.quimson777@gmail.com>
2023-11-14 20:58:06 +0100

    feat: funzione somma

--- esercizio2-paridispari/js/main.js
+++ esercizio2-paridispari/js/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function myRandom() {
-  const myNumber = Number(Math.floor(Math.random() * 5) + 1);
-  return myNumber;
+  const pcNumber = Number(Math.floor(Math.random() * 5) + 1);
+  return pcNumber;
+}
+
+function mySomma(userInputNum, pcNumber) {
+  const sum = userInputNum + pcNumber;
+  if (sum % 2 === 0) {
+    return 'PARI';
+  } else {
+    return 'DISPARI';
+  }
 }
 
 //Chiedo all'utente pari o dispari
@@ -16,5 +25,9 @@ let userInputNum = Number(prompt('Numero da 1 a 5'));
 console.log(`Hai scelto: ${userInputNum}`);
 
 //Numero random del PC
-const myNumber = myRandom();
-console.log(`Numero PC: ${myNumber}`);
+const pcNumber = myRandom();
+console.log(`Numero PC: ${pcNumber}`);
+
+//Richiamo la funzione della somma
+const risultato = mySomma(userInputNum, pcNumber);
+console.log(risultato);
