c60d9d6f87441fe689f616e629a7f3de39be2ea1
JohnQuimson <john.quimson777@gmail.com>
2023-11-14 18:40:08 +0100

    feat: prima metà della parola

--- esercizio1-palindroma/js/main.js
+++ esercizio1-palindroma/js/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function palindroma(parola) {
+  for (let i = 0; i < parola.length / 2; i++) {
+    console.log(i + '. ' + parola[i]);
+  }
+}
+
+const inpuUtente = prompt('Inserisci la parola');
