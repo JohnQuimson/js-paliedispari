601222eb3924d802fdde27d73e315fc8d4e0e5e7
JohnQuimson <john.quimson777@gmail.com>
2023-11-14 20:20:49 +0100

    feat: verifiche & README

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ### Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
 
 1. Creo una funzione e lavoro li dentro
-2. Inizializzo un promp in cui chiedo all'utente la parola
-3. confronto la parola di indice 0 con quella di indice -1 e così via
+
+2. con un ciclo for scorro tra le lettere della parola fino a metà
+3. Confronto con un if la prima lettera con l'ultima, se sono uguali mi da ritorno 'è palindroma', altrimenti 'non è palindroma'
+4. Inizializzo un promp in cui chiedo all'utente la parola
+5. creo una variabile in cui inserisco l'esito del return
+6. stampo la variabile
--- esercizio1-palindroma/js/main.js
+++ esercizio1-palindroma/js/main.js
@@ -1,12 +1,31 @@
 'use strict';
 
 function palindroma(parola) {
-  for (let i = 0; i < parola.length / 2; i++) {
-    if (parola[i] === parola[parola.length - 1 - i]) {
-      return 'La parola è palindroma';
+  // Verifico che non ci siano maiuscole
+  if (parola.toLowerCase() !== parola) {
+    alert('Non inserire maiuscole');
+    return 'Non inserire maiuscole';
+  }
+
+  // Creo una nuova parola senza spazi
+  let nuovaParola = '';
+  for (let k = 0; k < parola.length; k++) {
+    if (parola[k] !== ' ') {
+      nuovaParola += parola[k];
     }
   }
-  return 'La parola NON è palindroma';
+
+  // Riassegno nuovaParola a parola così il codice dopo non è da variare
+  parola = nuovaParola;
+  console.log(parola);
+
+  // Verifico se la parola è palindroma
+  for (let i = 0; i < parola.length / 2; i++) {
+    if (parola[i] !== parola[parola.length - 1 - i]) {
+      return 'La parola NON è palindroma';
+    }
+  }
+  return 'La parola è palindroma';
 }
 
 const inputUtente = prompt('Inserisci la parola');
