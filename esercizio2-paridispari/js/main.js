814c82d92d4305ff3add11d44976924a5ac2c698
JohnQuimson <john.quimson777@gmail.com>
2023-11-14 20:51:49 +0100

    feat: inputs

--- esercizio2-paridispari/js/main.js
+++ esercizio2-paridispari/js/main.js
@@ -5,5 +5,16 @@ function myRandom() {
   return myNumber;
 }
 
+//Chiedo all'utente pari o dispari
+let userInputPD = prompt('Pari o dispari ?');
+//Rendo l'input tutto minuscolo per evitare problemi
+userInputPD = userInputPD.toLowerCase();
+console.log(`Hai scelto: ${userInputPD}`);
+
+//Chiedo il numero all'utente
+let userInputNum = Number(prompt('Numero da 1 a 5'));
+console.log(`Hai scelto: ${userInputNum}`);
+
+//Numero random del PC
 const myNumber = myRandom();
-console.log(myNumber);
+console.log(`Numero PC: ${myNumber}`);
